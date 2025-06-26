2ea35de0b014176f3c5fcbebd8367b86336d2253
Al66li8 <a.mucixo.v727@edu.utc.mx>
2025-06-26 09:50:56 -0600

    libreria corns

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan= require(morgan);
-const cors = require(cors);
+const morgan= require('morgan');
+const cors = require('cors');
 
 // Configurar el servidor
 
